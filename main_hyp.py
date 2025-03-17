1c4f671f213ce22c58a52eccbbd9d10d6efa434d
rob <roberto.morelli7@unibo.it>
2025-03-17 10:09:14 +0100

    added data paraller support (to test on multigpu env)

--- main_hyp.py
+++ main_hyp.py
@@ -4,6 +4,7 @@ import wandb
 from utils.general import read_yaml
 from train import train
 from box import Box
+import subprocess
 from config import *
 
 AVAIL_GPUS = min(1, torch.cuda.device_count())
@@ -20,31 +21,38 @@ def main(args):
     model_cfg = Box(read_yaml(cfg_yaml))
     sweep_config['parameters']['cfg'] = {'value': model_cfg}  # Add dataset YAML dynamically
     sweep_config['parameters']['project_name'] = {'value': args.project_name}  # Add model dynamically
-    sweep_config['parameters']['devices'] = {'value': ','.join(str(i) for i in range(int(args.ngpus)))}
+    sweep_config['parameters']['devices'] = {'value': int(args.ngpus)}
     sweep_config['parameters']['ncpus'] = {'value': int(args.ncpus)}
     if args.group is not None:
         os.environ[ "WANDB_GROUP"] = args.group  # it cannot be added to sweep cfg because it need to init wandb before getting weep cfg (see train funct.)
 
     # Initialize the sweep
     # project is the name on wandb API
-    sweep_id = wandb.sweep(sweep_config, project=args.project_name, entity=args.entity)
-    print(f"SWEEP_ID: {sweep_id}")
-    os.environ["sweep_id"] = sweep_id
-    # Run the sweep
-    wandb.agent(sweep_id, function=train, count=args.exps_num)
+    if args.sweep_id is not None:
+        # Define the bash command
+        command = f"wandb agent robmorelli/segformer_hyp_opt_dv/{args.sweep_id}"
+        # Run the command using subprocess
+        subprocess.run(command, shell=True, check=True)
+    else:
+        sweep_id = wandb.sweep(sweep_config, project=args.project_name, entity=args.entity)
+        print(f"SWEEP_ID: {sweep_id}")
+        os.environ["sweep_id"] = sweep_id
+        # Run the sweep
+        wandb.agent(sweep_id, function=train, count=args.exps_num)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run YOLO sweep with wandb")
     parser.add_argument("--entity", default='robmorelli', help="Dataset configuration file (YAML)")
-    parser.add_argument("--project_name", default='segformer_hyp_opt',
+    parser.add_argument("--project_name", default='segformer_hyp_opt_test',
                         help="Dataset configuration file (YAML)")
     parser.add_argument("--conf_yaml", default='segformer', help="Dataset configuration file (YAML)")
     parser.add_argument("--sweep_cfg", default='sweep', help="Sweep configuration file (YAML)")
-    parser.add_argument("--ngpus", default=1, help="Sweep configuration file (YAML)")
-    parser.add_argument("--ncpus", default=6, help="Sweep configuration file (YAML)")
-    parser.add_argument("--exps_num", default=10, help="number of trials")
-    parser.add_argument("--group", default=None, help="number of trials")
+    parser.add_argument("--ngpus", default=2, help="")
+    parser.add_argument("--ncpus", default=6, help="")
+    parser.add_argument("--exps_num", default=10, help="")
+    parser.add_argument("--group", default=None, help="")
+    parser.add_argument("--sweep_id", default=None, help="")
 
     args, unknown = parser.parse_known_args()
     main(args)
--- train.py
+++ train.py
@@ -7,9 +7,53 @@ import wandb
 from box import Box
 from utils.training import load_model, create_dataloader
 from config import *
+import subprocess
+
+def get_gpu_memory():
+    """
+    Returns the memory usage of all available GPUs on the node.
+    Uses `nvidia-smi` to get the GPU memory stats.
+    """
+    gpu_info = []
+    try:
+        result = subprocess.check_output(
+            ['nvidia-smi', '--query-gpu=index,memory.free,memory.total', '--format=csv,nounits,noheader'])
+        result = result.decode('utf-8').strip().split('\n')
+        for line in result:
+            parts = line.split(', ')
+            gpu_index = int(parts[0])  # GPU index
+            memory_free = int(parts[1])  # Free memory (in MB)
+            memory_total = int(parts[2])  # Total memory (in MB)
+            memory_used = memory_total - memory_free
+            gpu_info.append(
+                (gpu_index, memory_used, memory_free, memory_total))  # (index, used memory, free memory, total memory)
+    except Exception as e:
+        print(f"Error retrieving GPU memory information: {e}")
+    return gpu_info
+
+
+def select_gpus(n=2):
+    """
+    Selects the N GPUs with the lowest memory usage (or highest available memory) for training.
+    :param n: Number of GPUs to select.
+    :return: A list of selected GPU ids.
+    """
+    # Get GPU memory information
+    gpu_info = get_gpu_memory()
+    if not gpu_info:
+        raise ValueError("No GPU information found.")
+
+    # Sort GPUs by free memory (descending), prioritize the ones with most free memory
+    sorted_gpus = sorted(gpu_info, key=lambda x: x[2], reverse=True)  # Sort by free memory
+
+    # Select the top N GPUs
+    selected_gpus = [gpu[0] for gpu in sorted_gpus[:n]]  # Select the top N GPUs
+
+    print(f"Selected GPUs based on memory: {selected_gpus}")
+    return selected_gpus
+
 
 def train():
-
     # Initialize wandb for this run
     group = os.getenv("WANDB_GROUP", None)
     if group is not None:
@@ -17,15 +61,12 @@ def train():
     else:
         run = wandb.init()
 
-    cfg =  Box(wandb.config.get('cfg'))
+    cfg = Box(wandb.config.get('cfg'))
     cfg.dataset.fold = wandb.config.get('fold', 1)
-    cfg.opt.weights =  wandb.config.get('classes_weights')
+    cfg.opt.weights = wandb.config.get('classes_weights')
     cfg.opt.lr = wandb.config.get('lr')
     cfg.opt.es_patience = wandb.config.get('es_patience', '7')
     cfg.opt.lr_patience = wandb.config.get('lr_patience', '3')
-
-    # Get an environment variable
-    device_env = os.getenv("GPU_ID")
 
     # Generate a unique timestamp for this training run
     now = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
@@ -38,73 +79,85 @@ def train():
     sweep = api.sweep(f"{entity}/{project}/{sweep_id}")
     sweep_name = sweep.name  # Retrieve the sweep name
 
-    # Define additional arguments
-    batch_size = wandb.config.get('batch', 12)  # Default batch size if not in config
-    device_id = wandb.config.get('devices', '0')
-    device = f"cuda:{device_id}" if torch.cuda.is_available() else "cpu"
+    fold = wandb.config.get('fold', '1')  # Default batch size if not in config
 
+    model_dir = os.path.join(wandb.config.get('project_name'), sweep_name, cfg.model.name, cfg.model.encoder_name,
+                             f"fold_{fold}",
+                             cfg.model.exp_name + f'_w_{cfg.opt.weight[0]}_{cfg.opt.weight[1]}_{cfg.opt.weight[2]}' + "_" + now,
+                             run.name)
+
+    #### INSTANTIATE MODEL ###
+    model = load_model(cfg, wandb.config.get('unfreeze'))
+
+    # Get an environment variable for device selection (if provided)
+    device_env = os.getenv("GPU_ID")
     if device_env:
-        device = device_env
+        device = f"cuda:{device_env}"
+        model = model.to(device)  # Move model to first selected GPU
+    else:
+        # Select the N GPUs with the least load (top N)
+        n_gpus = wandb.config.get('devices', 1)  # You can change N to the desired number of GPUs
+        selected_gpus = select_gpus(n=n_gpus)
+        # Convert selected GPUs into a format suitable for PyTorch DataParallel
+        device_ids = selected_gpus
+        os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, device_ids))
+
+        # If using multiple GPUs, use DataParallel
+        if len(device_ids) > 1:
+            print(f"Using GPUs {device_ids} for training")
+            model = torch.nn.DataParallel(model, device_ids=device_ids)
+        else:
+            model = model.to(f"cuda:{device_ids[0]}")  # Move model to first selected GPU
+        device = f"cuda:{device_ids[0]}"  # Set the device to the first GPU for criterion and other tasks
 
     # Configure CPU threading
     ncpus = int(wandb.config.get('ncpus', 0))
     if ncpus == 0:
-        ncpus = int(device[-2])*12
+        ncpus = int(device[-2]) * 12
     torch.set_num_threads(ncpus)
     num_workers = ncpus
+
+    # Adjust batch size based on the number of GPUs
+    batch_size = wandb.config.get('batch', 12) * n_gpus  # Default batch size if not in config
 
     os.environ["OMP_NUM_THREADS"] = str(ncpus)
     os.environ["MKL_NUM_THREADS"] = str(ncpus)
 
-    fold = wandb.config.get('fold', '1')  # Default batch size if not in config
-
-    model_dir = os.path.join(wandb.config.get('project_name'), sweep_name, cfg.model.name, cfg.model.encoder_name, f"fold_{fold}"
-                             , cfg.model.exp_name + f'_w_{cfg.opt.weight[0]}_{cfg.opt.weight[1]}_{cfg.opt.weight[2]}'+ "_" + now, run.name)
-
-    #### INSTANTIATE MODEL ###
-    model = load_model(cfg, wandb.config.get('unfreeze'))
-    model.to(device)
-    #### GET DATALOADERS ###
+    # Get DataLoader
     train_dataloader, val_dataloader = create_dataloader(cfg, batch_size=batch_size, num_workers=num_workers)
-    ### TRAIN ###
 
+    ### LOSS FUNCTION ###
     if cfg.opt.crossentropy_loss:
         if cfg.opt.weights is not None:
-            if cfg.opt.weights == 0:
-                raise NotImplementedError
             weights = cfg.opt.weights
             weights = weights if isinstance(weights, torch.FloatTensor) else torch.FloatTensor(weights)
             if cfg.opt.ignore_index is not None:
-                criterion = torch.nn.CrossEntropyLoss(weight=weights, ignore_idex=cfg.opt.ignore_index).to(
-                    device)  # weight (Tensor, optional): a manual rescaling weight given
-                # to each class as to be a Tensor of size `C` and floating point dtype
+                criterion = torch.nn.CrossEntropyLoss(weight=weights, ignore_index=cfg.opt.ignore_index).to(device)
             else:
-                criterion = torch.nn.CrossEntropyLoss(weight=weights).to(
-                    device)  # weight (Tensor, optional): a manual rescaling weight given
-                # to each class as to be a Tensor of size `C` and floating point dtype
-                print('crossentropy with pos weights')
+                criterion = torch.nn.CrossEntropyLoss(weight=weights).to(device)
         else:
             if cfg.opt.ignore_index is not None:
-                criterion = torch.nn.CrossEntropyLoss(ignore_index=cfg.opt.ignore_index)
+                criterion = torch.nn.CrossEntropyLoss(ignore_index=cfg.opt.ignore_index).to(device)
             else:
-                criterion = torch.nn.CrossEntropyLoss()
+                criterion = torch.nn.CrossEntropyLoss().to(device)
     else:
-        criterion = torch.nn.BCELoss()
-        print('BCE')
+        criterion = torch.nn.BCELoss().to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.opt.lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.8, patience=cfg.opt.lr_patience,
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.8,
+                                                           patience=cfg.opt.lr_patience,
                                                            threshold=0.0001, threshold_mode='rel', cooldown=0,
                                                            min_lr=9e-8, verbose=True)
     early_stopping = EarlyStopping(patience=cfg.opt.es_patience)
 
+    # Start training
     training_cycle_segformer_multiclass(cfg=cfg, model=model, train_loader=train_dataloader,
-                                      val_loader=val_dataloader,
-                                      criterion=criterion, optimizer=optimizer
-                                      , scheduler=scheduler, early_stopping=early_stopping,
-                                      model_name=cfg.model.name,
-                                      out_dir=model_dir, device=device,
-                                      num_epochs=wandb.config.get('epochs'))
+                                        val_loader=val_dataloader,
+                                        criterion=criterion, optimizer=optimizer,
+                                        scheduler=scheduler, early_stopping=early_stopping,
+                                        model_name=cfg.model.name, out_dir=model_dir,
+                                        device=device, num_epochs=wandb.config.get('epochs'))
+
 
 if __name__ == "__main__":
     train()
